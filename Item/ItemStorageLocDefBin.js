b46e5a8cbf198694a1cad2d7424fdf3b2984100e
yunawinaya <gedeyuna@gmail.com>
2026-06-05 12:58:44 +0800

    update

--- Item/ItemStorageLocDefBin.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-  const storageLocationId = arguments[0].value;
-  const rowIndex = arguments[0].rowIndex;
-
-  if (storageLocationId) {
-    await this.disabled([`table_default_bin.${rowIndex}.bin_location`], false);
-  }
-})();
