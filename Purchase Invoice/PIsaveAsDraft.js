a24ec6ee0418bac2445cc74047371781208b55d7
yunawinaya <gedeyuna@gmail.com>
2026-06-15 11:44:29 +0800

    feat: implement PI save as draft function and configure SI post workflow automation

---
+++ Purchase Invoice/PIsaveAsDraft.js
@@ -0,0 +1,36 @@
+const closeDialog = () => {
+  if (this.parentGenerateForm) {
+    this.parentGenerateForm.$refs.SuPageDialogRef.hide();
+    this.parentGenerateForm.refresh();
+    this.hideLoading();
+  }
+};
+
+const submitForm = async (data) => {
+  await this.runWorkflow(
+    "2066342495378059266",
+    {
+      data: data,
+    },
+    (res) => {
+      this.$message.success(`${this.isEdit ? "Update" : "Add"} successfully`);
+      closeDialog();
+    },
+    (error) => {
+      this.hideLoading();
+      console.error(error);
+      const message = error.data?.msg || "An error occurred";
+      this.$message.error(message);
+    },
+  );
+};
+
+(async () => {
+  this.showLoading("Saving Purchase Invoice...");
+  let data = this.getValues();
+
+  data.pi_status = "Draft";
+  data.posted_status = "";
+
+  await submitForm(data);
+})();
